647d73edc20206cecd25cf25bd74d1c372f98d7d
HamedPro3D <110119103+HamedPro3D@users.noreply.github.com>
2022-10-07 12:22:47 -0500

    Actualizacion Punto2

--- Punto2.py
+++ Punto2.py
@@ -1,10 +1,13 @@
+from asyncore import write
 import csv
+comp = False
 print("Solo escribir s para sí, n para no")
 escuela = input("¿Quiere un vecindario con escuela? ")
 gimnasio = input("¿Quiere un vecindario con gimansio? ")
 bar = input("¿Quiere un vecindario con bar? ")
 tienda = input("¿Quiere un vecindario con tienda? ") 
 parque = input("¿Quiere un vecindario con parque? ")
+acc = list()
 with open("data.csv") as f:
     lector = csv.reader(f)
     for row in lector:
@@ -12,7 +15,8 @@ with open("data.csv") as f:
         if(row[1] == "1"):
             row[1] = "Si"
         if(row[1] == "0"):
-            row[1] = "No"    
+            row[1] = "No"
+            comp = True    
         if(row[2] == "1"):
             row[2] = "Si"
         if(row[2] == "0"):
@@ -29,9 +33,15 @@ with open("data.csv") as f:
             row[5] = "Si"
         if(row[5] == "0"):
             row[5] = "No"
+        acc.append(row)
         print("Vecindario: {0}, Escuela: {1}, Gimnasio: {2}, Bar: {3}, Tienda: {4}, Parque: {5} ".format(row[0],row[1],row[2],row[3],row[4],row[5]))
-    with open("lista.csv","w",newline="") as file:
-        escritor = csv.writer(file)
-        if(escuela == "n"):
-            escritor.writerow((row[0],escuela,gimnasio,bar,tienda,parque))
+    
+
+with open("lista.csv","w") as file:
+    escritor = csv.writer(file)
+    for row in acc:
+        if(escuela == "n" and comp == True):
+            escritor.writerow((row[0],row[1],row[2],row[3],row[4],row[5]))
+    
+        
 
